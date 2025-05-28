3def0c64bf875fe61de566cc7c6a34bac71e1e50
Piotrek Rybiec <piotrrybiec28@gmail.com>
2025-05-28 23:10:06 +0200

    scraper and stored tasks dto, copy types

--- packages/nest/src/rest/dtos.ts
+++ packages/nest/src/rest/dtos.ts
@@ -1,3 +1,25 @@
+export type Maybe<T> = T | null;
+export type InputMaybe<T> = Maybe<T>;
+export type Exact<T extends { [key: string]: unknown }> = { [K in keyof T]: T[K] };
+export type MakeOptional<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]?: Maybe<T[SubKey]> };
+export type MakeMaybe<T, K extends keyof T> = Omit<T, K> & { [SubKey in K]: Maybe<T[SubKey]> };
+
+export type Scalars = {
+  ID: string;
+  String: string;
+  Boolean: boolean;
+  Int: number;
+  Float: number;
+  Date: Date;
+  DateTime: Date;
+  EmailAddress: string;
+  JSON: Record<string, unknown> | Array<unknown> | string | number | boolean | null;
+  JWT: string;
+  PositiveInt: number;
+  URL: string;
+  UUID: string;
+};
+
 export interface GoogleAuthDto {
     code: string;
 }
@@ -31,6 +53,17 @@ export interface ScheduleEventDto {
   next?: ScheduleEventDto;
 }
 
+export interface StoredTaskDto {
+    createdAt: Scalars['DateTime'];
+    finalHash?: Maybe<string>;
+    finishedAt?: Maybe<Scalars['DateTime']>;
+    id: Scalars['ID'];
+    initialHash?: Maybe<string>;
+    scraper?: Maybe<ScraperDto>;
+    //status: TaskStateDto;
+    status: string;
+}
+
 export interface TasksBulkDto {
     count: number;
     offset?: number;
@@ -45,3 +78,10 @@ export interface FinishTaskDto {
     result: string[], // maybe taskState?
     scraperId?: string;
 } 
+
+export interface ScraperDto {
+    id: string;
+    alias: string;
+    lastSeen?: Scalars['DateTime'];
+    state: string;
+}
